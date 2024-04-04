2fc638e5906f16d4f2399e329140c0ec11eb9b1e
Wojciech Biegała <wojciech.biegala@gmail.com>
2024-04-04 21:09:54 +0200

    Small fix 7

--- src/ReceiptAnalyzer.Shared/Extensions/StringExtensions.cs
+++ src/ReceiptAnalyzer.Shared/Extensions/StringExtensions.cs
@@ -9,7 +9,11 @@ namespace BS.ReceiptAnalyzer.Shared.Extensions
             StringBuilder sb = new StringBuilder();
             foreach (char c in str)
             {
-                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+                if ((c >= '0' && c <= '9')
+                    || (c >= 'A' && c <= 'Z')
+                    || (c >= 'a' && c <= 'z')
+                    || c == '.'
+                    || c == '_')
                 {
                     sb.Append(c);
                 }
