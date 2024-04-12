35a59127f4a3afde041d5d4f93ce531f214f1376
Wojciech Biegała <wojciech.biegala@gmail.com>
2024-04-12 22:26:35 +0200

    Analysis tak progress notification
    TODO: rewrite consummer to MassTransit

--- src/ReceiptAnalyzer.Core/Commands/NotifyAnalysisTaskProgress/NotifyAnalysisTaskProgressCommandHandler.cs
+++ src/ReceiptAnalyzer.Core/Commands/NotifyAnalysisTaskProgress/NotifyAnalysisTaskProgressCommandHandler.cs
@@ -1,12 +1,36 @@
-﻿using MediatR;
+﻿using BS.ReceiptAnalyzer.Data;
+using BS.ReceiptAnalyzer.Domain.Model;
+using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace BS.ReceiptAnalyzer.Core.Commands.NotifyAnalysisTaskProgress
 {
     internal class NotifyAnalysisTaskProgressCommandHandler : IRequestHandler<NotifyAnalysisTaskProgressCommand>
     {
-        public Task Handle(NotifyAnalysisTaskProgressCommand request, CancellationToken cancellationToken)
+        private readonly IAnalysisTaskRepository _repository;
+        private readonly ILogger<NotifyAnalysisTaskProgressCommandHandler> _logger;
+
+        public NotifyAnalysisTaskProgressCommandHandler(IAnalysisTaskRepository repository,
+            ILogger<NotifyAnalysisTaskProgressCommandHandler> logger)
         {
-            throw new NotImplementedException();
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task Handle(NotifyAnalysisTaskProgressCommand command, CancellationToken cancellationToken)
+        {
+            var task = await _repository.GetById(command.TaskId);
+
+            if (task == null)
+                return;
+
+            var step = (AnalysisTaskProgression)command.Step;
+
+            var notification = AnalysisTaskStep.Create(step, command.Success,
+                DateTimeOffset.Now, command.StartTime, command.EndTime);
+            task.NotifyStepFinished(notification);
+
+            await _repository.UnitOfWork.CommitChangesAsync(cancellationToken);
         }
     }
 }
