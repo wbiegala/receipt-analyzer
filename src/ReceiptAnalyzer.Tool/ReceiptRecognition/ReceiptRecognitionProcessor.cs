9cf801ce372f803b7da237d171b8e1e5b7eeec52
Wojciech Biegała <wojciech.biegala@gmail.com>
2024-04-21 21:49:17 +0200

    Images cutting tool - work 2

--- src/ReceiptAnalyzer.Tool/ReceiptRecognition/ReceiptRecognitionProcessor.cs
+++ src/ReceiptAnalyzer.Tool/ReceiptRecognition/ReceiptRecognitionProcessor.cs
@@ -5,15 +5,34 @@ namespace BS.ReceiptAnalyzer.Tool.ReceiptRecognition
     internal class ReceiptRecognitionProcessor : IImagesProcessor
     {
         private readonly IReceiptRecognizerService _recognizer;
+        private readonly string _inputDirectory;
 
-        public ReceiptRecognitionProcessor(IReceiptRecognizerService recognizer)
+        public ReceiptRecognitionProcessor(IReceiptRecognizerService recognizer, ProgramArgs config)
         {
             _recognizer = recognizer ?? throw new ArgumentNullException(nameof(recognizer));
+            _inputDirectory = config.InputDirectory;
         }
 
         public async Task ProcessAsync()
         {
-            throw new NotImplementedException();
+            var files = GetFiles();
+
+            foreach (var file in files)
+            {
+                //TODO: processing
+            }
+        }
+
+
+
+        private IEnumerable<string> GetFiles()
+        {
+            var result = new List<string>();
+            result.AddRange(Directory.GetFiles(_inputDirectory, "*.png"));
+            result.AddRange(Directory.GetFiles(_inputDirectory, "*.jpg"));
+            result.AddRange(Directory.GetFiles(_inputDirectory, "*.jpeg"));
+
+            return result;
         }
     }
 }
