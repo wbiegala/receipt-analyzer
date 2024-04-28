72d81fe508fdfa8e5090eb357b1f6386a80bca31
Wojciech Biegała <wojciech.biegala@gmail.com>
2024-04-28 23:14:22 +0200

    Small fix 16

--- src/ReceiptAnalyzer.Local/Program.cs
+++ src/ReceiptAnalyzer.Local/Program.cs
@@ -17,8 +17,6 @@ namespace ReceiptAnalyzer.Local
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
